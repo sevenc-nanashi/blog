1043fe2a9b8b4f033e75f0022531c8393a705baf
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-07-26 22:29:01 +0900

    Add: 目次の表示を改善

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -23,58 +23,61 @@ export function TableOfContents({ toc }) {
       </div>
       <nav
         className={[
-          "lg:min-w-[20%] lg:w-max w-screen h-screen lg:h-full drop-shadow-xl lg:drop-shadow-none",
-          "t-0 fixed lg:relative -translate-y-24 lg:translate-y-0 pointer-events-none lg:pointer-events-auto",
-          isOpen || "-translate-x-full lg:translate-x-0 shadow-md lg:shadow-sm",
-          isOpen && "pointer-events-auto z-20 bg-theme-pale",
+          "lg:min-w-[20%] lg:w-max w-screen h-screen lg:h-auto lg:min-h-full drop-shadow-xl lg:drop-shadow-none",
+          "t-0 fixed lg:relative -translate-y-24 lg:-translate-y-0 pointer-events-none lg:pointer-events-auto",
+          "transition-transform",
+          isOpen || "-translate-x-full lg:translate-x-0",
+          isOpen && "pointer-events-auto z-20",
         ].join(" ")}
         onClick={() => setOpen(false)}
       >
-        <div
-          className={[
-            "flex flex-col content-bg rounded-r lg:rounded p-4 h-full fixed lg:static top-0 left-0",
-            isOpen ||
-              "-translate-x-full lg:translate-x-0 shadow-md lg:shadow-sm",
-            isOpen && "w-2/3",
-          ]
-            .filter((c) => typeof c === "string")
-            .join(" ")}
-        >
-          <ul className="">
-            <li className="text-theme text-xl font-bold">
-              <Link href="#root" passHref>
-                <a>目次</a>
-              </Link>
-            </li>
-            {items.map((item) => {
-              const href = `#${item.id}`
-              const active =
-                typeof window !== "undefined" && window.location.hash === href
-              return (
-                <li
-                  key={item.title}
-                  className={[
-                    active ? "text-theme font-bold" : undefined,
-                    item.level === 3 ? "pl-4" : undefined,
-                    "text-lg",
-                  ]
-                    .filter(Boolean)
-                    .join(" ")}
-                >
-                  <Link href={href} passHref>
-                    <a
-                      onClick={() => {
-                        setOpen(false)
-                      }}
-                      className="hover:text-theme"
-                    >
-                      {item.title}
-                    </a>
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
+        <div className="lg:sticky w-full h-max top-8">
+          <div
+            className={[
+              "flex flex-col content-bg rounded-r lg:rounded p-4 h-full lg:h-max fixed lg:relative top-0 left-0",
+              isOpen ||
+                "-translate-x-full lg:translate-x-0 shadow-md lg:shadow-sm",
+              isOpen && "w-2/3",
+            ]
+              .filter((c) => typeof c === "string")
+              .join(" ")}
+          >
+            <ul className="pl-4 lg:pl-0">
+              <li className="text-theme text-2xl lg:text-xl font-bold">
+                <Link href="#root" passHref>
+                  <a>目次</a>
+                </Link>
+              </li>
+              {items.map((item) => {
+                const href = `#${item.id}`
+                const active =
+                  typeof window !== "undefined" && window.location.hash === href
+                return (
+                  <li
+                    key={item.title}
+                    className={[
+                      active ? "text-theme font-bold" : undefined,
+                      item.level === 3 ? "pl-4" : undefined,
+                      "text-lg",
+                    ]
+                      .filter(Boolean)
+                      .join(" ")}
+                  >
+                    <Link href={href} passHref>
+                      <a
+                        onClick={() => {
+                          setOpen(false)
+                        }}
+                        className="hover:text-theme"
+                      >
+                        {item.title}
+                      </a>
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
       </nav>
     </>
